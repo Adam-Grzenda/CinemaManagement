6ba6cc4fdbfbd9796badb53374acd65dffed722d
Adam-Grzenda <65490875+Adam-Grzenda@users.noreply.github.com>
2021-12-10 20:28:31 +0100

    Initial commit

---
+++ src/test/java/pl/put/CinemaManagement/CinemaManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.put.CinemaManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
