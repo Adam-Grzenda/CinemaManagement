b03101770219e6e1c7a3ec6c128104514f94e235
matfrac <mateusz.p.frackowiak@gmail.com>
2021-12-11 19:51:34 +0100

    changes

--- src/main/java/pl/put/CinemaManagement/repository/CinemaRepository.java
+++ src/main/java/pl/put/CinemaManagement/repository/CinemaRepository.java
@@ -3,5 +3,9 @@ package pl.put.CinemaManagement.repository;
 import org.springframework.data.repository.CrudRepository;
 import pl.put.CinemaManagement.model.Cinema;
 
+import java.util.List;
+
 public interface CinemaRepository extends CrudRepository<Cinema, Long> {
+
+    List<Cinema> findCinemaByName(String name);
 }
