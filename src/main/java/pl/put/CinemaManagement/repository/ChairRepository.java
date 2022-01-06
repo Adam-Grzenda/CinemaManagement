f7bfdd26ac9ba30d816b55aa57a295ae839727e4
adamg <adam.grzenda@gmail.com>
2022-01-06 01:47:37 +0100

    add custom query for free chairs on film show

---
+++ src/main/java/pl/put/CinemaManagement/repository/ChairRepository.java
@@ -0,0 +1,15 @@
+package pl.put.CinemaManagement.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import pl.put.CinemaManagement.model.Chair;
+
+import java.util.List;
+
+public interface ChairRepository extends CrudRepository<Chair, Long> {
+    @Query("FROM Chair c1" +
+            " WHERE c1.id NOT in (select c.id from Chair c inner join Ticket t on c.id = t.chair.id where t.filmShow.id = :filmShowId) ")
+    List<Chair> findFreeChairsForShow(@Param("filmShowId") Long filmShowId);
+
+}
