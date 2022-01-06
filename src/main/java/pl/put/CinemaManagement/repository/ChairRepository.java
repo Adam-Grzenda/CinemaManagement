a7115fd57af3700cbcca32f532a0f0faf22f6f58
matfrac <mateusz.p.frackowiak@gmail.com>
2022-01-07 00:58:04 +0100

    change free seat finding query

--- src/main/java/pl/put/CinemaManagement/repository/ChairRepository.java
+++ src/main/java/pl/put/CinemaManagement/repository/ChairRepository.java
@@ -8,8 +8,8 @@ import pl.put.CinemaManagement.model.Chair;
 import java.util.List;
 
 public interface ChairRepository extends CrudRepository<Chair, Long> {
-    @Query("FROM Chair c1" +
-            " WHERE c1.id NOT in (select c.id from Chair c inner join Ticket t on c.id = t.chair.id where t.filmShow.id = :filmShowId) ")
+    @Query("FROM Chair c1 WHERE c1.cinemaHall.id = (SELECT f.cinemaHall.id FROM FilmShow f WHERE f.id = :filmShowId) AND" +
+            " c1.id NOT in (select c.id from Chair c inner join Ticket t on c.id = t.chair.id where t.filmShow.id = :filmShowId) ")
     List<Chair> findFreeChairsForShow(@Param("filmShowId") Long filmShowId);
 
 }
