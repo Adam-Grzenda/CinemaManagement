3c0b331348d76c49443f7fb9a7140815a898ea57
adamg <adam.grzenda@gmail.com>
2022-02-19 15:48:03 +0100

    expose ID for data rest api

--- src/main/java/pl/put/CinemaManagement/security/RestApiConfig.java
+++ src/main/java/pl/put/CinemaManagement/security/RestApiConfig.java
@@ -1,17 +1,26 @@
 package pl.put.CinemaManagement.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.Type;
 import java.util.Arrays;
 
+@RequiredArgsConstructor
 @Configuration
-public class CorsFilterConfig {
+public class RestApiConfig {
+
+    private final EntityManager entityManager;
 
     @Value("${client.host}:${client.port}")
     private String clientHost;
@@ -32,5 +41,16 @@ public class CorsFilterConfig {
         filter.setOrder(0);
         return filter;
     }
+
+    @Bean
+    public RepositoryRestConfigurer repositoryRestConfigurer() {
+        return new RepositoryRestConfigurer() {
+            @Override
+            public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+                config.exposeIdsFor(entityManager.getMetamodel().getEntities().stream().map(Type::getJavaType).toArray(Class[]::new));
+            }
+        };
+    }
+
 
 }
